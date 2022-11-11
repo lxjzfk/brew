fa088412c9f8388d2e85bbc0d979b06e8eab09f4
lxjzfk <lxjzfk@tuta.io>
2022-11-11 14:58:26 +1000

    Brew model & string representations

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class Brew(models.Model):
+    name = models.CharField(max_length=80)
+    date = models.DateField(auto_now=False, auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.date + self.name
+
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=40)
 
+    def __str__(self) -> str:
+        return self.name
